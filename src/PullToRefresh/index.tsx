21ea01399309d372c9771ac3ee81b0d1b19b9fae
eraylee <eraylee@live.com>
2022-04-06 23:16:53 +0800

    feat(PullToRefresh): ✨ basic realization

--- src/PullToRefresh/index.tsx
+++ src/PullToRefresh/index.tsx
@@ -5,21 +5,14 @@ import { useSpring, animated } from 'react-spring';
 import { bem } from '../utils/bem';
 import { useScrollTarget } from '../hooks/scroll';
 import { getScrollTop } from '../utils/scroll';
-const isWindow = (node: Window | HTMLElement): node is Window => node === window;
 
 export enum RefreshStatus {
   None,
   Pulling,
-  Disentangling,
+  release,
   Refreshing,
 }
 
-const config = {
-  [RefreshStatus.None]: '',
-  [RefreshStatus.Pulling]: '继续下拉...',
-  [RefreshStatus.Disentangling]: '松开以刷新...',
-  [RefreshStatus.Refreshing]: '刷新中...',
-};
 export interface PullToRefreshProps {
   /**
    * @description.zh-CN 组件额外的 className
@@ -27,17 +20,53 @@ export interface PullToRefreshProps {
    * @default
    */
   className?: string;
+  /**
+   * @description.zh-CN 拖动触发更新距离
+   * @description.en-US Pull to trigger update distance
+   * @default
+   */
   distance?: number;
+  /**
+   * @description.zh-CN  是否禁用拖动功能
+   * @description.en-US Whether to disable the drag function
+   * @default
+   */
+  disabled?: boolean;
+  /**
+   * @description.zh-CN  下拉状态提示文本
+   * @description.en-US Pulling status prompt text
+   * @default
+   */
+  pullingText?: string | JSX.Element;
+  /**
+   * @description.zh-CN  释放状态提示文本
+   * @description.en-US Release status prompt text
+   * @default
+   */
+  releaseText?: string | JSX.Element;
+  /**
+   * @description.zh-CN  刷新状态提示文本
+   * @description.en-US Refreshing status prompt text
+   * @default
+   */
+  refreshingText?: string | JSX.Element;
+  /**
+   * @description.zh-CN  刷新事件
+   * @description.en-US Refresh event
+   * @default
+   */
   onRefresh?: () => Promise<void>;
-  refreshContent?: JSX.Element;
 }
 
 export const PullToRefresh: FC<PullToRefreshProps> = ({
   className,
+  disabled,
   onRefresh,
   distance,
   children,
-  refreshContent,
+  pullingText,
+  releaseText,
+  refreshingText,
 }) => {
   const ref = useRef<HTMLDivElement>(null);
   const [status, setStatus] = useState(RefreshStatus.None);
@@ -46,60 +75,76 @@ export const PullToRefresh: FC<PullToRefreshProps> = ({
   const isReachTop = useRef(false);
   const scrollTarget = useScrollTarget(ref.current);
   const [{ y }, api] = useSpring(() => ({ y: 0 }));
-
+  const pullable = useMemo(
+    () => !disabled && status !== RefreshStatus.Refreshing,
+    [status, disabled],
+  );
   const handleTouchStart: TouchEventHandler<HTMLDivElement> = (e) => {
-    setStatus(RefreshStatus.Pulling);
-    isReachTop.current = getScrollTop(scrollTarget) === 0;
-    if (isReachTop.current) {
-      beginRef.current = e.touches[0].clientY;
+    if (pullable) {
+      setStatus(RefreshStatus.Pulling);
+      isReachTop.current = getScrollTop(scrollTarget) === 0;
+      if (isReachTop.current) {
+        beginRef.current = e.touches[0].clientY;
+      }
     }
   };
 
   const handleTouchMove: TouchEventHandler<HTMLDivElement> = (e) => {
-    if (isReachTop.current) {
+    if (pullable && isReachTop.current) {
       const moveY = e.touches[0].clientY - beginRef.current;
       const offset = moveY > (distance as number) ? distance || 0 : moveY;
       api.start({ y: offset });
       if (offset === distance) {
-        setStatus(RefreshStatus.Disentangling);
+        setStatus(RefreshStatus.release);
       }
     }
   };
 
   const handleTouchEnd: TouchEventHandler<HTMLDivElement> = async (e) => {
-    if (isReachTop.current) {
-      setStatus(RefreshStatus.Refreshing);
-      onRefresh && (await onRefresh());
+    if (pullable && isReachTop.current) {
+      if (status === RefreshStatus.release) {
+        setStatus(RefreshStatus.Refreshing);
+        onRefresh && (await onRefresh());
+      }
       api.start({ y: 0 });
       beginRef.current = 0;
       setStatus(RefreshStatus.None);
     }
   };
+  // 提示内容
+  const refreshHeaderInfo = useMemo(() => {
+    const map = {
+      [RefreshStatus.None]: '',
+      [RefreshStatus.Pulling]: pullingText,
+      [RefreshStatus.release]: releaseText,
+      [RefreshStatus.Refreshing]: refreshingText,
+    };
+    return <div className={clsx(bem(prefix, 'head', 'info'))}>{map[status]}</div>;
+  }, [status]);
 
-  const _refreshContent = useMemo(() => {
-    if (refreshContent) {
-      return refreshContent;
-    }
-    return <span>{config[status]}</span>;
-  }, [refreshContent, status]);
   return (
-    <animated.div
-      css={style}
-      style={{ y }}
-      ref={ref}
-      className={clsx(prefix, className)}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
+    <div css={style} className={clsx(prefix, className)}>
       <animated.div style={{ height: y }} className={bem(prefix, 'head')}>
-        {_refreshContent}
+        {refreshHeaderInfo}
       </animated.div>
-      <div>{children}</div>
-    </animated.div>
+      <animated.div
+        style={{ y }}
+        ref={ref}
+        className={clsx(bem(prefix, 'wrapper'))}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {children}
+      </animated.div>
+    </div>
   );
 };
 
 PullToRefresh.defaultProps = {
-  distance: 100,
+  distance: 60,
+  disabled: false,
+  pullingText: '拉动以刷新...',
+  releaseText: '松开以刷新...',
+  refreshingText: '刷新中...',
 };
--- src/PullToRefresh/style.ts
+++ src/PullToRefresh/style.ts
@@ -1,13 +1,29 @@
-import { jsx, css } from '@emotion/react';
+import { css } from '@emotion/react';
 
 export const prefix = 'rescroll-pull-to-refresh';
 
 export const style = css`
   .${prefix} {
+    position: relative;
     &_head {
-      display: flex;
-      align-items: center;
-      justify-content: center;
+      position: absolute;
+      top: 0;
+      width: 100%;
+      overflow: hidden;
+      background-color: whitesmoke;
+
+      /* background-color: red; */
+      &_info {
+        display: flex;
+        align-items: flex-end;
+        justify-content: center;
+        box-sizing: border-box;
+        height: 100%;
+        padding-bottom: 8px;
+        font-size: 12px;
+      }
+    }
+    &_wrapper {
     }
   }
 `;
